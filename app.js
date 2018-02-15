f01f15b98bd69ba77c05db4de1f6d374a2a0ce86
Andrew Fidel <Drew@Andrews-MacBook-Pro-2.local>
2018-02-14 17:48:34 -0800

    new repo project

---
+++ app.js
@@ -0,0 +1,13 @@
+var myfaker = require('faker');//refers to faker package
+
+// console.log(myfaker.address.city());
+
+function generateAddress() {
+    console.log(myfaker.address.streetAddress());
+    console.log(myfaker.address.city());
+    console.log(myfaker.address.state());
+}
+
+generateAddress();
+
+    
